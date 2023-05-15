ec441af7da37ac4e026ac53e9b76b4f3741fe7a1
Ahmed Elzeiny <zeinyman@gmail.com>
2023-05-15 08:35:13 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace TestComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lookUpEdit1 = new DevExpress.XtraEditors.LookUpEdit();
+            this.lookUpEdit2 = new DevExpress.XtraEditors.LookUpEdit();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.searchLookUpEdit1 = new DevExpress.XtraEditors.SearchLookUpEdit();
+            this.searchLookUpEdit1View = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchLookUpEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchLookUpEdit1View)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lookUpEdit1
+            // 
+            this.lookUpEdit1.Location = new System.Drawing.Point(12, 12);
+            this.lookUpEdit1.Name = "lookUpEdit1";
+            this.lookUpEdit1.Properties.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lookUpEdit1.Properties.Appearance.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lookUpEdit1.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lookUpEdit1.Properties.Appearance.Options.UseBackColor = true;
+            this.lookUpEdit1.Properties.Appearance.Options.UseFont = true;
+            this.lookUpEdit1.Properties.AppearanceDropDown.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lookUpEdit1.Properties.AppearanceDropDown.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lookUpEdit1.Properties.AppearanceDropDown.Font = new System.Drawing.Font("Tahoma", 9.75F);
+            this.lookUpEdit1.Properties.AppearanceDropDown.Options.UseBackColor = true;
+            this.lookUpEdit1.Properties.AppearanceDropDown.Options.UseFont = true;
+            this.lookUpEdit1.Properties.BestFitMode = DevExpress.XtraEditors.Controls.BestFitMode.BestFitResizePopup;
+            this.lookUpEdit1.Properties.BestFitRowCount = 5;
+            this.lookUpEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lookUpEdit1.Properties.DropDownRows = 3;
+            this.lookUpEdit1.Properties.PopupFilterMode = DevExpress.XtraEditors.PopupFilterMode.Contains;
+            this.lookUpEdit1.Properties.PopupWidthMode = DevExpress.XtraEditors.PopupWidthMode.ContentWidth;
+            this.lookUpEdit1.Size = new System.Drawing.Size(210, 22);
+            this.lookUpEdit1.TabIndex = 0;
+            // 
+            // lookUpEdit2
+            // 
+            this.lookUpEdit2.Location = new System.Drawing.Point(12, 49);
+            this.lookUpEdit2.Name = "lookUpEdit2";
+            this.lookUpEdit2.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lookUpEdit2.Size = new System.Drawing.Size(210, 20);
+            this.lookUpEdit2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // searchLookUpEdit1
+            // 
+            this.searchLookUpEdit1.Location = new System.Drawing.Point(59, 157);
+            this.searchLookUpEdit1.Name = "searchLookUpEdit1";
+            this.searchLookUpEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.searchLookUpEdit1.Properties.PopupView = this.searchLookUpEdit1View;
+            this.searchLookUpEdit1.Size = new System.Drawing.Size(268, 20);
+            this.searchLookUpEdit1.TabIndex = 4;
+            // 
+            // searchLookUpEdit1View
+            // 
+            this.searchLookUpEdit1View.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.searchLookUpEdit1View.Name = "searchLookUpEdit1View";
+            this.searchLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.searchLookUpEdit1View.OptionsView.ShowGroupPanel = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchLookUpEdit1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lookUpEdit2);
+            this.Controls.Add(this.lookUpEdit1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchLookUpEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchLookUpEdit1View)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LookUpEdit lookUpEdit1;
+        private DevExpress.XtraEditors.LookUpEdit lookUpEdit2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private DevExpress.XtraEditors.SearchLookUpEdit searchLookUpEdit1;
+        private DevExpress.XtraGrid.Views.Grid.GridView searchLookUpEdit1View;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Data.DataTable dt = new DataTable();
+            dt.Columns.Add("ItemName", typeof(string));
+            dt.Columns.Add("ItemCode", typeof(string));
+            dt.Columns.Add("ItemId", typeof(int));
+            dt.Columns.Add("ItemPrice", typeof(decimal));
+
+            dt.Rows.Add("ItemA", "A", 1, 14.5);
+            dt.Rows.Add("ItemB", "B", 2, 0.75);
+            dt.Rows.Add("ItemC", "C", 3, 1);
+
+
+
+            List<MyItems> lst = new List<MyItems>();
+            lst.Add(new MyItems { MyItemName = "x1", MyItemCode = "x1", MyItemId = 1, MyItemPrice = 18 });
+            lst.Add(new MyItems { MyItemName = "x2", MyItemCode = "x2", MyItemId = 2, MyItemPrice = 181 });
+            lst.Add(new MyItems { MyItemName = "x3", MyItemCode = "x3", MyItemId = 3, MyItemPrice = 19 });
+            lst.Add(new MyItems { MyItemName = "x4", MyItemCode = "x4", MyItemId = 4, MyItemPrice = 8 });
+
+            lookUpEdit2.Properties.DataSource = lst;
+            lookUpEdit2.Properties.ValueMember = "MyItemId";
+            lookUpEdit2.Properties.DisplayMember = "MyItemName";
+
+            lookUpEdit1.Properties.DataSource = dt;
+            lookUpEdit1.Properties.PopulateColumns();
+            lookUpEdit1.Properties.DisplayMember = "ItemName";
+            lookUpEdit1.Properties.ValueMember = "ItemId";
+
+            lookUpEdit1.Properties.Columns["ItemId"].Visible = false;
+            lookUpEdit1.Properties.Columns["ItemName"].Caption = @"اسم الصنف";
+
+            lookUpEdit1.Properties.NullText = @"الرجاء اختر الصنف أولا";
+
+
+            lookUpEdit1.CustomDisplayText += LookUpEdit1_CustomDisplayText;
+
+            lookUpEdit1.Properties.Columns["ItemPrice"].AllowSort = DevExpress.Utils.DefaultBoolean.False;
+            lookUpEdit1.Properties.Columns["ItemPrice"].Alignment = DevExpress.Utils.HorzAlignment.Near;
+            lookUpEdit1.ButtonClick += LookUpEdit1_ButtonClick;
+            var btnclear = new DevExpress.XtraEditors.Controls.EditorButton
+            {
+                Kind = DevExpress.XtraEditors.Controls.ButtonPredefines.Clear,
+                IsLeft = false,
+                Tag = "Clear",
+                Shortcut = new DevExpress.Utils.KeyShortcut(Keys.Control | Keys.Delete)
+                
+            };
+            var imgbtn = new DevExpress.XtraEditors.Controls.EditorButton
+            {
+                Kind = DevExpress.XtraEditors.Controls.ButtonPredefines.Glyph,
+                IsLeft = false,
+                Tag = "Add",
+                Shortcut = new DevExpress.Utils.KeyShortcut(Keys.Shift | Keys.A)
+                , ToolTip= "To Add A new Item (CTRL+A)"
+
+            };
+            imgbtn.ImageOptions.ImageUri = @"Add;Size16x16";
+            lookUpEdit1.Properties.Buttons.AddRange(new[] { imgbtn, btnclear });
+
+
+            searchLookUpEdit1.Properties.DataSource=dt;
+            searchLookUpEdit1.Properties.DisplayMember = "ItemName";
+            searchLookUpEdit1.Properties.ValueMember = "ItemId";
+
+        }
+
+        private void LookUpEdit1_ButtonClick(object sender, DevExpress.XtraEditors.Controls.ButtonPressedEventArgs e)
+        {
+            if (!e.Button.Enabled || e.Button.Tag == null) return;
+
+
+            if (string.Equals(e.Button.Tag.ToString(), "clear", StringComparison.InvariantCultureIgnoreCase))
+            {
+                lookUpEdit1.EditValue = null;
+            }
+            else if (string.Equals(e.Button.Tag.ToString(), "Add", StringComparison.InvariantCultureIgnoreCase))
+            {
+                DataTable dt = (DataTable)lookUpEdit1.Properties.DataSource;
+                dt.Rows.Add("NewOne" + dt.Rows.Count, "Code" + dt.Rows.Count, dt.Rows.Count, dt.Rows.Count * 1.2);
+                
+            }
+        }
+
+        private void LookUpEdit1_CustomDisplayText(object sender, DevExpress.XtraEditors.Controls.CustomDisplayTextEventArgs e)
+        {
+            if (lookUpEdit1.GetSelectedDataRow() == null) e.DisplayText = lookUpEdit1.Properties.NullText;
+            else
+            {
+                var ItemName = MyClass.GetColumnValue(lookUpEdit1, "ItemName").ToString();
+                var ItemPrice = Convert.ToDecimal(MyClass.GetColumnValue(lookUpEdit1, "ItemPrice"));
+                e.DisplayText = $@"{ItemName} ({ItemPrice:0.00})";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            MessageBox.Show(MyClass.GetColumnValue(lookUpEdit1)?.ToString());
+            MessageBox.Show(MyClass.GetColumnValue(lookUpEdit1, "ItemCode")?.ToString());
+
+            var choice = MyClass.GetColumnValue<MyItems>(lookUpEdit2) as MyItems;
+            if (choice != null)
+            {
+                MessageBox.Show(choice.MyItemCode);
+                MessageBox.Show(choice.MyItemPrice.ToString());
+                MessageBox.Show(choice.ToString());
+            }
+
+            //if (lookUpEdit1.GetSelectedDataRow() != null)
+            //{
+            //    if (lookUpEdit1.GetSelectedDataRow() is DataRowView myrow)
+            //    {
+            //        MessageBox.Show($@"Code={myrow["ItemCode"]}, Price={myrow["ItemPrice"]}");
+            //    }
+            //}
+            //if (lookUpEdit2.GetSelectedDataRow() is MyItems item)
+            //{
+            //    MessageBox.Show($@"Code={item.MyItemCode}, Price={item.MyItemPrice}");
+
+            //}
+
+
+            //}
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+
+            //if (lookUpEdit1.GetSelectedDataRow() == null)
+            //{
+            //    MessageBox.Show(lookUpEdit1.Properties.NullText);
+            //    lookUpEdit1.ShowPopup();
+            //    return;
+            //}
+
+            lookUpEdit1.EditValue = null;
+            //lookUpEdit2.EditValue = 2;
+        }
+    }
+
+    public class MyItems
+    {
+        public string MyItemName { get; set; }
+        public string MyItemCode { get; set; }
+        public int MyItemId { get; set; }
+        public decimal MyItemPrice { get; set; }
+
+        public override string ToString()
+        {
+            return $@"ItemName={MyItemName},Code={MyItemCode},Price={MyItemPrice:0.##} ";
+        }
+    }
+    internal class MyClass
+    {
+        public static object GetColumnValue(LookUpEdit look, string columnName = null)
+        {
+            if (look.GetSelectedDataRow() == null) return null;
+
+            //if (string.IsNullOrEmpty(columnName) || string.IsNullOrWhiteSpace(columnName)) return null;
+            //if (look.Properties.Columns?.Count == 0) return null;
+            //string valuemember = columnName == null ? look.Properties.ValueMember : columnName;
+            string valuemember = columnName ?? look.Properties.ValueMember;
+
+            if (look.Properties.Columns is { Count: > 0 } && look.Properties.Columns[valuemember] != null)
+            {
+                return ((DataRowView)look.GetSelectedDataRow())[valuemember];
+            }
+            return null;
+
+        }
+        public static object GetColumnValue<MyType>(LookUpEdit look)
+        {
+            if (look.GetSelectedDataRow() == null) return null;
+            if (look.GetSelectedDataRow() is MyType) return (MyType)look.GetSelectedDataRow();
+            return null;
+
+        }
+    }
+}
